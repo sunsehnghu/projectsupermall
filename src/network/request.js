0319f4b3682c14109e3acced925e7e01c9d9c61f
sunsehnghu <2401998201@qq.com>
2020-09-27 22:24:26 +0800

    我提交到本地仓库了

---
+++ src/network/request.js
@@ -0,0 +1,46 @@
+import axios from 'axios'
+
+// export function requset(congig){
+//     return new Promise((resolve,reject)=>{
+//     //创建axios的实例
+//     const instance =axios.create({
+//         baseURL:'"http://152.136.185.210:8000/api/z8',
+//         timeout:5000
+//     })
+//     //发送真正的网络请求
+//     instance(config)
+//        .then(res=>{
+//            resolve(res)
+//        })
+//        .catch(err=>{
+//            reject(err)
+//        })
+//     })
+// }
+export function request(config){
+    
+    //创建axios的实例
+    const instance =axios.create({
+        baseURL:'http://152.136.185.210:8000/api/z8',
+        timeout:5000
+    })
+
+
+    //axios的拦截器
+    instance.interceptors.request.use(config=>{
+        
+        return config
+    },err=>{
+        console.log(err);
+    })
+    //相应拦截
+    instance.interceptors.response.use(res=>{
+        return res.data
+    },err=>{
+        console.log(err);
+    })
+    //发送真正的网络请求
+     return instance(config)
+       
+    
+}
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,49 @@
 <template>
-    <div>
-        <h2>首页</h2>
+    <div id="home">
+        <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+        <swiper>
+            <swiper-item v-for="(item,i) in banners" :key="item.index">
+                <a :href="item.link">
+                    <img :src="item.image" alt="">
+                </a>
+            </swiper-item>
+            
+        </swiper>
     </div>
 </template>
 <script>
+   import NavBar from 'components/common/navbar/NavBar'
+   import {getHomeMultidata} from 'network/home'
+   import Swiper from 'components/common/swiper/Swiper'
+   import SwiperItem from 'components/common/swiper/SwiperItem'
 export default {
-    name:"Home"
+    name:"Home",
+    components:{
+        NavBar,
+        Swiper,
+        SwiperItem
+    },
+    data() {
+        return {
+            // result:null
+            banners:[],
+            recommends:[]
+        }
+    },
+    created() {
+        //1.请求多个数据
+        getHomeMultidata().then(res=>{
+            // console.log(res);
+            // this.result=res;
+            this.banners=res.data.banner.list,
+            this.recommends=res.data.recommend.list
+        })
+    },
 }
 </script>
 <style scoped>
-    
+    .home-nav{
+        background-color: var(--color-tint);
+        color:#fff
+    }
 </style>
