f5ada4d6404995eff8727f305e89fc25864ad9f7
sunsehnghu <2401998201@qq.com>
2020-09-28 23:26:30 +0800

    我提交到本地仓库了

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,15 +4,70 @@
         <home-swiper :banners="banners"></home-swiper>
         <recommend-view :recommends="recommends"></recommend-view>
         <feature-view/>
-        
+        <tab-control class="tab-control"    :titles="['流行','新款','精选']"/>
+        <ul>
+            <li>列表1</li>
+            <li>列表2</li>
+            <li>列表3</li>
+            <li>列表4</li>
+            <li>列表5</li>
+            <li>列表6</li>
+            <li>列表7</li>
+            <li>列表8</li>
+            <li>列表9</li>
+            <li>列表10</li>
+            <li>列表11</li>
+            <li>列表12</li>
+            <li>列表13</li>
+            <li>列表14</li>
+            <li>列表15</li>
+            <li>列表16</li>
+            <li>列表17</li>
+            <li>列表18</li>
+            <li>列表19</li>
+            <li>列表20</li>
+            <li>列表21</li>
+            <li>列表22</li>
+            <li>列表23</li>
+            <li>列表24</li>
+            <li>列表25</li>
+            <li>列表26</li>
+            <li>列表27</li>
+            <li>列表28</li>
+            <li>列表29</li>
+            <li>列表30</li>
+            <li>列表31</li>
+            <li>列表32</li>
+            <li>列表33</li>
+            <li>列表34</li>
+            <li>列表35</li>
+            <li>列表36</li>
+            <li>列表37</li>
+            <li>列表38</li>
+            <li>列表39</li>
+            <li>列表40</li>
+            <li>列表41</li>
+            <li>列表42</li>
+            <li>列表43</li>
+            <li>列表44</li>
+            <li>列表45</li>
+            <li>列表46</li>
+            <li>列表47</li>
+            <li>列表48</li>
+            <li>列表49</li>
+            <li>列表50</li>
+        </ul>
     </div>
 </template>
 <script>
-   import NavBar from 'components/common/navbar/NavBar'
+   
    import  HomeSwiper from './childComps/HomeSwiper'
    import RecommendView from './childComps/RecommendView'
    import FeatureView from './childComps/FeatureView'
-   import {getHomeMultidata} from 'network/home'
+
+   import NavBar from 'components/common/navbar/NavBar'
+   import TabControl from 'components/content/tabControl/TabControl'
+   import {getHomeMultidata,getHomeGoods} from 'network/home'
   
 export default {
     name:"Home",
@@ -20,23 +75,44 @@ export default {
         NavBar,
         HomeSwiper,
         RecommendView,
-        FeatureView
+        FeatureView,
+        TabControl
     },
     data() {
         return {
             // result:null
             banners:[],
-            recommends:[]
+            recommends:[],
+            goods:{
+                'pop':{page:0,list:[]},
+                'new':{page:0,list:[]},
+                'sell':{page:0,list:[]}
+            }
         }
     },
     created() {
         //1.请求多个数据
-        getHomeMultidata().then(res=>{
+        this.getHomeMultidata()
+        this.getHomeGoods('pop')
+        this.getHomeGoods('new')
+        this.getHomeGoods('sell')
+    },
+    methods: {
+        getHomeMultidata(){
+            getHomeMultidata().then(res=>{
             // console.log(res);
             // this.result=res;
             this.banners=res.data.banner.list,
             this.recommends=res.data.recommend.list
         })
+        },
+        getHomeGoods(type){
+            const page =this.goods[type].page+1
+            getHomeGoods(type,page).then(res=>{
+              this.goods[type].list.push(...res.data.list)
+              this.goods[type].page +=1
+        })
+        }
     },
 }
 </script>
@@ -52,5 +128,9 @@ export default {
         right: 0;
         top: 0;
         z-index: 9;
+    }
+    .tab-control{
+        position: sticky;
+        top: 44px;
     }
 </style>
