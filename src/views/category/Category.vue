eb1f63f6cf69b1636049af519b9019ff77443824
sunsehnghu <2401998201@qq.com>
2020-10-10 21:30:41 +0800

    我提交到本地仓库了

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,143 +1,130 @@
 <template>
-    <div class="wrapper">
-        <ul class="content">
-            <button @click="btnclick">按钮</button>
-            <li>列表1</li>
-            <li>列表2</li>
-            <li>列表3</li>
-            <li>列表4</li>
-            <li>列表5</li>
-            <li>列表6</li>
-            <li>列表7</li>
-            <li>列表8</li>
-            <li>列表9</li>
-            <li>列表10</li>
-            <li>列表11</li>
-            <li>列表12</li>
-            <li>列表13</li>
-            <li>列表14</li>
-            <li>列表15</li>
-            <li>列表16</li>
-            <li>列表17</li>
-            <li>列表18</li>
-            <li>列表19</li>
-            <li>列表20</li>
-            <li>列表21</li>
-            <li>列表22</li>
-            <li>列表23</li>
-            <li>列表24</li>
-            <li>列表25</li>
-            <li>列表26</li>
-            <li>列表27</li>
-            <li>列表28</li>
-            <li>列表29</li>
-            <li>列表30</li>
-            <li>列表31</li>
-            <li>列表32</li>
-            <li>列表33</li>
-            <li>列表34</li>
-            <li>列表35</li>
-            <li>列表36</li>
-            <li>列表37</li>
-            <li>列表38</li>
-            <li>列表39</li>
-            <li>列表40</li>
-            <li>列表41</li>
-            <li>列表42</li>
-            <li>列表43</li>
-            <li>列表44</li>
-            <li>列表45</li>
-            <li>列表46</li>
-            <li>列表47</li>
-            <li>列表48</li>
-            <li>列表49</li>
-            <li>列表50</li>
-            <li>列表51</li>
-            <li>列表52</li>
-            <li>列表53</li>
-            <li>列表54</li>
-            <li>列表55</li>
-            <li>列表56</li>
-            <li>列表57</li>
-            <li>列表58</li>
-            <li>列表59</li>
-            <li>列表60</li>
-            <li>列表61</li>
-            <li>列表62</li>
-            <li>列表63</li>
-            <li>列表64</li>
-            <li>列表65</li>
-            <li>列表66</li>
-            <li>列表67</li>
-            <li>列表68</li>
-            <li>列表69</li>
-            <li>列表70</li>
-            <li>列表71</li>
-            <li>列表72</li>
-            <li>列表73</li>
-            <li>列表74</li>
-            <li>列表75</li>
-            <li>列表76</li>
-            <li>列表77</li>
-            <li>列表78</li>
-            <li>列表79</li>
-            <li>列表80</li>
-            <li>列表81</li>
-            <li>列表82</li>
-            <li>列表83</li>
-            <li>列表84</li>
-            <li>列表85</li>
-            <li>列表86</li>
-            <li>列表87</li>
-            <li>列表88</li>
-            <li>列表89</li>
-            <li>列表90</li>
-            <li>列表91</li>
-            <li>列表92</li>
-            <li>列表93</li>
-            <li>列表94</li>
-            <li>列表95</li>
-            <li>列表96</li>
-            <li>列表97</li>
-            <li>列表98</li>
-            <li>列表99</li>
-            <li>列表100</li>
-        </ul>
+    <div class="category">
+        <nav-bar class="nav-bar">
+            <div slot="center">商品分类</div>
+        </nav-bar>
+        <div class="content">
+        <tab-menu :categories="categories" @selectItem="selectItem"></tab-menu>
+        <tab-content-category class="tab-content-category" :subcategories="showSubcategory"></tab-content-category>
+        </div>
     </div>
+
+    
 </template>
 <script>
+import NavBar from 'components/common/navbar/NavBar'
+import TabMenu from './childComps/TabMenu'
+import TabContentCategory from './childComps/TabContentCategory'
 
-import BScroll from 'better-scroll'
+import{getCategory,getSubcategory,getCategoryDetail} from 'network/category'
+
 export default {
     name:"Category",
+    components:{
+        NavBar,
+        TabMenu,
+        TabContentCategory
+    },
     data() {
         return {
-            scroll:null
+            categories:[],
+            categoryData:{},
+            currentIndex:-1,
+            
+            
+
         }
     },
-    mounted() {
-        this.scroll =new BScroll(document.querySelector('.wrapper'),{
-            probeType:3,
-            pullUpLoad:true
-        })
-        this.scroll.on('scroll',(position)=>{
-            console.log(position);
-        })
-        this.scroll.on('pullingUp',()=>{
-            console.log('上拉加载更多');
-        })
+    created() {
+        //请求分类数据
+        this._getCategory()
+    },
+    computed: {
+		  showSubcategory() {
+		    // if (this.currentIndex === -1) return {}
+        return this.categoryData[this.currentIndex].subcategories
+      },
     },
     methods: {
-        btnclick(){
-            console.log('bthlick');
-        }
+        _getCategory(){
+            getCategory().then(res=>{
+                console.log(res);
+                //获取分类数据
+                this.categories =res.data.category.list
+                // 2.初始化每个类别的子数据
+                for(let i =0;i<this.categories.length;i++){
+                    this.categoryData[i]={
+                        subcategories:{},
+                        //  categoryDetail:{
+                        //     'pop':[],
+                        //     'new':[],
+                        //     'sell':[]
+                        //  }
+                    
+                     }
+                }
+                //请求第一个分类的数据
+                this._getSubcategories(0)
+            })
+        },
+        _getSubcategories(index){
+             this.currentIndex=index
+             const mailKey=this.categories[index].maitKey;
+             getSubcategory(mailKey).then(res=>{
+                 console.log(res);
+                 this.categoryData[index].subcategories=res.data
+                  this.categoryData = {...this.categoryData}
+                 
+
+
+
+             })
+
+        },
+        // _getCategoryDetail(type){
+        //         //获取请求的miniWallkey
+        //         const minWallkey =this.categories[this.currentIndex].miniWallkey
+        //         //2.发送请求，传入miniWallkey和type
+        //         getCategoryDetail(miniWallkey,type).then(res=>{
+        //              console.log(res);
+        //         })
+
+        // },
+         /*事件响应相关的方法*/
+    selectItem(index){
+        this._getSubcategories(index)
+    }
     },
+   
 }
+    
 </script>
 <style scoped>
-    .wrapper{
-        height: 200px;
-        background-color: red;
-        overflow: hidden;
-    }
+.category{
+    height: 100vh;
+}
+     .nav-bar{
+         background-color: var(--color-tint);
+         color: #fff;
+         height: 44px;
+         position: flex;
+         top: 0;
+         right: 0;
+         left: 0;
+         
+
+     }
+     .tab-content-category{
+         height: 100%;
+         flex: 1;
+     }
+      .content {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 44px;
+    bottom: 49px;
+
+    display: flex;
+  }
 </style>
---
+++ src/views/category/childComps/TabMenu.vue
@@ -0,0 +1,65 @@
+<template>
+<scroll id="tab-menu" :pullUpLoad="true">
+    <div class="menu-list">
+    <div class="menu-list-item" 
+    :class="{active: index===currentIndex}"
+    v-for="(item,index) in categories" :key="item.index"
+    
+    @click="itemClick(index)">
+        {{item.title}}
+    </div>
+    </div>
+</scroll>
+</template>
+<script>
+import Scroll from 'components/common/scroll/Scroll'
+export default {
+    name:"TabMenu",
+    components:{
+        Scroll
+    },
+    data() {
+        return {
+            currentIndex:0
+        }
+    },
+    props:{
+        categories:{
+            type:Array,
+            
+        }
+    },
+    methods: {
+        itemClick(index){
+            this.currentIndex=index
+            this.$emit('selectItem',index)
+        }
+    },
+    
+
+    
+}
+</script>
+
+<style scoped>
+#tab-menu{
+    height: 100%;
+    background-color: #f6f6f6;
+    width: 100px;
+    box-sizing: border-box;
+    overflow: hidden;
+    
+}
+    .menu-list-item{
+        height: 45px;
+        line-height: 45px;
+        text-align: center;
+        font-size: 14px;
+    }
+    .menu-list-item.active{
+        font-weight: 700;
+    color: var(--color-high-text);
+    background-color: #fff;
+    border-left: 3px solid var(--color-high-text);
+    }
+</style>
